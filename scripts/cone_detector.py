bb8e5846bbe283796f37706a0b168745afd7fa3b
hturner08 <hmturner@mit.edu>
2022-03-05 18:58:24 +0000

    Added rotation to fix flipped ZED Camera

--- scripts/cone_detector.py
+++ scripts/cone_detector.py
@@ -2,12 +2,13 @@
 
 import numpy as np
 import rospy
+import imutils
 
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 
 from sensor_msgs.msg import Image
-from geometry_msgs.msg import Point #geometry_msgs not in CMake file
+from geometry_msgs.msg import Point  # geometry_msgs not in CMake file
 from visual_servoing.msg import ConeLocationPixel
 
 # import your color segmentation algorithm; call this function in ros_image_callback!
@@ -20,15 +21,19 @@ class ConeDetector():
     Subscribes to: /zed/zed_node/rgb/image_rect_color (Image) : the live RGB image from the onboard ZED camera.
     Publishes to: /relative_cone_px (ConeLocationPixel) : the coordinates of the cone in the image frame (units are pixels).
     """
+
     def __init__(self):
         # toggle line follower vs cone parker
         self.LineFollower = False
 
         # Subscribe to ZED camera RGB frames
-        self.cone_pub = rospy.Publisher("/relative_cone_px", ConeLocationPixel, queue_size=10)
-        self.debug_pub = rospy.Publisher("/cone_debug_img", Image, queue_size=10)
-        self.image_sub = rospy.Subscriber("/zed/zed_node/rgb/image_rect_color", Image, self.image_callback)
-        self.bridge = CvBridge() # Converts between ROS images and OpenCV Images
+        self.cone_pub = rospy.Publisher(
+            "/relative_cone_px", ConeLocationPixel, queue_size=10)
+        self.debug_pub = rospy.Publisher(
+            "/cone_debug_img", Image, queue_size=10)
+        self.image_sub = rospy.Subscriber(
+            "/zed/zed_node/rgb/image_rect_color", Image, self.image_callback)
+        self.bridge = CvBridge()  # Converts between ROS images and OpenCV Images
 
     def image_callback(self, image_msg):
         # Apply your imported color segmentation function (cd_color_segmentation) to the image msg here
@@ -45,7 +50,7 @@ class ConeDetector():
         #################################
 
         image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
-
+        image = imutils.rotate(image, 180)  # ZED camers are upside down
         debug_msg = self.bridge.cv2_to_imgmsg(image, "bgr8")
         self.debug_pub.publish(debug_msg)
 
